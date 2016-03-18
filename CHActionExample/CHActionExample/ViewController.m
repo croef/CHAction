36aac9d4772f659e11edbc6dd29488e5e02bd77a
croef <changrong185@gmail.com>
2016-03-19 03:13:43 +0800

    fix demo bug

--- CHActionExample/CHActionExample/ViewController.m
+++ CHActionExample/CHActionExample/ViewController.m
@@ -104,7 +104,7 @@
         dispatch_async(dispatch_get_global_queue(0, 0), ^{
             NSLog(@"%@",action);
             sleep(5);
-            action.stop = YES;
+            [action cancel];
             NSLog(@"2%@",[NSThread currentThread]);
             finishBlock(@"c");
         });
